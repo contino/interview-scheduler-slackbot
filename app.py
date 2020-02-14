f92874ad549767f034dbbc244a2ce9cd16f86830
ashokgadeking <ashok.gadepalli@contino.io>
2020-02-13 22:44:49 -0600

    Read json from file

--- app.py
+++ app.py
@@ -21,32 +21,6 @@ def verify_slack_token(request_token):
         print("Received {} but was expecting {}".format(request_token, SLACK_VERIFICATION_TOKEN))
         return make_response("Request contains invalid Slack verification token", 403)
 
-# The endpoint Slack will load your menu options from
-@app.route("/slack/message_options", methods=["POST"])
-def message_options():
-    # Parse the request payload
-    form_json = json.loads(request.form["payload"])
-
-    # Verify that the request came from Slack
-    verify_slack_token(form_json["token"])
-
-    # Dictionary of menu options which will be sent as JSON
-    menu_options = {
-        "options": [
-            {
-                "text": "Cappuccino",
-                "value": "cappuccino"
-            },
-            {
-                "text": "Latte",
-                "value": "latte"
-            }
-        ]
-    }
-
-    # Load options dict as JSON and respond to Slack
-    return Response(json.dumps(menu_options), mimetype='application/json')
-
 # The endpoint Slack will send the user's menu selection to
 @app.route("/slack/message_actions", methods=["POST"])
 def message_actions():
@@ -81,116 +55,27 @@ def message_actions():
     # Send an HTTP 200 response with empty body so Slack knows we're done here
     return make_response("", 200)
 
-# with open('botkit_message.json') as msg_file:
-#     intro_msg = json.load(msg_file)
-#     print(intro_msg)
+with open('botkit_message.json') as msg_file:
+    intro_msg = json.load(msg_file)
 
-intro_msg  = json.dumps([
-        {
-            "blocks": [
-                {
-                    "type": "section",
-                    "text": {
-                        "type": "mrkdwn",
-                        "text": "Hello, Contini! I checked your interview availability calendar and it looks like you have not added your availability next week (Feb 10 to 14 2020). Your contribution really matters! \n\n *Please select one or more slots from below and I will create an event on the Interview availability calendar for you! :smiley:*"
-                    }
-                },
-                {
-                    "type": "divider"
-                },
-                {
-                    "type": "section",
-                    "text": {
-                        "type": "mrkdwn",
-                        "text": "Monday 3pm - 4pm EST LC first round."
-                    },
-                    "accessory": {
-                        "type": "button",
-                        "text": {
-                            "type": "plain_text",
-                            "text": "Signup"
-                        },
-                        "value": "click_me_123"
-                    }
-                },
-                {
-                    "type": "section",
-                    "text": {
-                        "type": "mrkdwn",
-                        "text": "Tuesday 10am - 11am EST LC first round. "
-                    },
-                    "accessory": {
-                        "type": "button",
-                        "text": {
-                            "type": "plain_text",
-                            "text": "Signup"
-                        },
-                        "value": "click_me_123"
-                    }
-                },
-                {
-                    "type": "section",
-                    "text": {
-                        "type": "mrkdwn",
-                        "text": "Wednesday 9am - 10am EST LC first round."
-                    },
-                    "accessory": {
-                        "type": "button",
-                        "text": {
-                            "type": "plain_text",
-                            "text": "Signup"
-                        },
-                        "value": "click_me_123"
-                    }
-                },
-                {
-                    "type": "section",
-                    "text": {
-                        "type": "mrkdwn",
-                        "text": "Wednesday 11am - 12pm EST LC first round."
-                    },
-                    "accessory": {
-                        "type": "button",
-                        "text": {
-                            "type": "plain_text",
-                            "text": "Signup"
-                        },
-                        "value": "click_me_123"
-                    }
-                },
-                {
-                    "type": "section",
-                    "text": {
-                        "type": "mrkdwn",
-                        "text": "Friday 3pm - 4pm EST LC first round."
-                    },
-                    "accessory": {
-                        "type": "button",
-                        "text": {
-                            "type": "plain_text",
-                            "text": "Signup"
-                        },
-                        "value": "click_me_123"
-                    }
-                }
-            ]
-        }
-    ])
 
-slack_client.api_call(
+payload = slack_client.api_call(
   "chat.postMessage",
-  channel="UTU6JPJG6",
+  channel="#trashbin",
   attachments=intro_msg
 )
 
+json_formatted_str = json.dumps(payload, indent=2)
+print(json_formatted_str)
+
 # print json.dumps(slack_client.api_call("users.list"), indent=2)
 
-payload = slack_client.api_call("users.list")
-json_formatted_str = json.dumps(payload, indent=2)
-# print(json_formatted_str)
+# payload = slack_client.api_call("users.list")
+# json_formatted_str = json.dumps(payload, indent=2)
+# # print(json_formatted_str)
 
-for item in payload["members"]:
-  print item["id"] + " " + item["profile"]["real_name_normalized"]
+# for item in payload["members"]:
+#   print item["id"] + " " + item["profile"]["real_name_normalized"]
 
 # Start the Flask server
 # if __name__ == "__main__":
